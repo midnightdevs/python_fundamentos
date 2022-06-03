f4e42209a09bf5109520c62f44d596512217e206
marcelfox <marcelfox@live.com>
2022-06-03 17:58:41 -0300

    melhorando o programa

--- primeiro_programa/funcao_principal.py
+++ primeiro_programa/funcao_principal.py
@@ -13,6 +13,7 @@ def cadastro():
     nome = input('Informe o nome: ')
     sexo = input('Informe o sexo:')
     idade = int(input('Informe a idade: '))
+    print('\n')
 
     return {
         "nome": nome,
@@ -22,9 +23,10 @@ def cadastro():
 
 def salva_clientes(num_clientes=3):
     lista_clientes = []
-    for num in num_clientes:
+    for num in range(num_clientes):
         cliente = cadastro()
         lista_clientes.append(cliente)
+    lista_clientes = ordena_por_idade(lista_clientes)
 
     return lista_clientes
 
@@ -39,4 +41,5 @@ def outra_funcao():
     print('Chamando outra função')
 
 if __name__ == '__main__':
-    funcao_principal()
+    lista_clientes = salva_clientes()
+    print(lista_clientes)
